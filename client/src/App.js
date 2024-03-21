ac9d0ef58a81ee5670c28ed3082ceee87b545a6b
code_hopper <ameykhondekar01@gmail.com>
2024-03-21 18:58:51 +0530

    created raect-router-dom for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
-import axios from "axios"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// importing pages
+import Home from "./components/pages/Home"
+import Contact from "./components/pages/Contact"
+import Store from "./components/pages/Store"
+import CustomerLR from "./components/pages/CustomerLR"
+import Product from "./components/pages/Product"
 
 // importing bootstarp
 import "bootstrap/dist/css/bootstrap.css"
@@ -8,35 +14,19 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 const App = () => {
 
-  let response;
-
-  let [message, setMessage] = useState(" ")
-
-  let pingBackend = async () => {
-    try {
-      response = await axios({
-        method: "GET",
-        url: "http://localhost:5000/api/test"
-      })
-
-      console.log(response)
-
-      setMessage(response.data.message)
-      console.log(response.data.message)
-    } catch (err) {
-      console.log(`error while ping the backend ${err} !`)
-    }
-  }
-
-  pingBackend()
-
   return (
     <>
-      {/* browser router dom */}
+      {/* creating router for frontend */}
 
-      <h1>Hello This is Frontend App.jsx</h1>
-
-      <h2>{message}</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/customerlr" element={<CustomerLR />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
---
+++ client/src/components/pages/CustomerLR.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerLR = () => {
+  return (
+    <div>CustomerLR</div>
+  )
+}
+
+export default CustomerLR
